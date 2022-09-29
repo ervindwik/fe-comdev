7a3d0755c143cdd2d899deb89b136626d4b9755b
nrmia19 <miaanr20@gmail.com>
2022-09-29 16:57:22 +0700

    chore: added page pengaturan profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
+import PengaturanProfile from "./Pages/PengaturanProfile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/pengaturanprofile" element={<PengaturanProfile/>} />
     </Routes>
   );
 }
