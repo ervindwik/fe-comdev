7887c8a0879ffc5daf9d07f7f66a250e889a358d
Nisa Alivia <nisa.alivia28@gmail.com>
2022-09-30 11:41:41 +0700

    add setting password page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 import Profile from "./Pages/Profile";
 import SettingProfile from "./Pages/SettingProfile";
+import SettingPassword from "./Pages/SettingPassword";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/profile/settingprofile" element={<SettingProfile/>} />
+      <Route path="/profile/settingprofile" element={<SettingProfile />} />
+      <Route path="/profile/settingpassword" element={<SettingPassword />} />
     </Routes>
   );
 }
