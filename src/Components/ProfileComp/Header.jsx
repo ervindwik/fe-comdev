a1305811dac97c18ca7d14b0d0f26cf02f074d1b
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2022-09-30 22:55:23 +0700

    Footer Location icon right side description fix

--- src/Components/ProfileComp/Header.jsx
+++ src/Components/ProfileComp/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import "../../Styles/Profile.css"
-import Coba from "../../assets/img_profile/coba.jpg"
+import Coba from "../../assets/img_profile/coba.jpeg"
 import Course from "../../assets/course.png"
 import Start from "../../assets/start.png"
 const Header = () => {
@@ -13,8 +13,8 @@ const Header = () => {
             <img className="css-img" src={Coba} />
             </div>
             <div className='css-borderDesc'>
-               <h2>MR. LOREM IPSUM SIR AMET</h2>
-               <h3>O YA NDAK TAHU</h3>
+               <h2>MISS SIRAMET</h2>
+               <h3>PENUNGGU KOLONG KASUR DAN KAMAR MANDI</h3>
                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro earum nesciunt quod aut impedit eveniet alias sapiente numquam iusto praesentium eos vitae, totam ratione facilis voluptatibus! Voluptatum cumque vero fuga?</p>
             </div>
           </div>
