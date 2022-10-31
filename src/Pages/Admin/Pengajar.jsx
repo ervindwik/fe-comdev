2c2639696198689241f1b25c6d0777a4fd4d1f5e
rifki alamsyah <m.rifki_si19@nusaputra.ac.id>
2022-10-31 09:32:49 +0700

    perbaikan sidebar admin agar tidak menyatu dengan landingpage

--- src/Pages/Admin/Pengajar.jsx
+++ src/Pages/Admin/Pengajar.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-
+import NavbarAdmin from '../../Components/AdminComp/Navbar';
+import SidebarAdmin from '../../Components/AdminComp/Sidebar';
 const Pengajar = () => {
-    return <h1>Ini halaman Pengajar</h1>;
+    return (
+        <>
+            <NavbarAdmin />
+            <div className='d-flex'>
+                <SidebarAdmin />
+                <h1>Ini halaman Home</h1> ;
+            </div>
+        </>
+    )
+
 }
 
 export default Pengajar
--- src/Pages/Admin/User.jsx
+++ src/Pages/Admin/User.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
-
+import NavbarAdmin from '../../Components/AdminComp/Navbar';
+import SidebarAdmin from '../../Components/AdminComp/Sidebar';
 const User = () => {
-    return <h1>Ini halaman User</h1>;
+    return (
+        <>
+            <NavbarAdmin />
+            <div className='d-flex'>
+                <SidebarAdmin />
+                <h1>Ini halaman User</h1> ;
+            </div>
+        </>
+    )
 }
 
 export default User
