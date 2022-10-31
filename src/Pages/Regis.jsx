9118e82c59806bd8a73fd8a85045723fe0dfee14
Nisa Alivia <nisa.alivia28@gmail.com>
2022-11-01 06:18:44 +0700

    regis integration with mock api

--- src/Pages/Regis.jsx
+++ src/Pages/Regis.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import { React, useState } from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import background from "../assets/logo.svg";
 import "../Styles/Regis.css";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Regis = () => {
+function Regis() {
+    const navigate = useNavigate()
+    const [data, setData] = useState({
+        email: "",
+        first_name: "",
+        last_name: "",
+        username: "",
+        password: "",
+        confirm_pass: ""
+    });
+
+    function registration(e){ 
+        e.preventDefault()
+        if(data.password === data.confirm_pass) {
+            delete data.confirm_pass
+            axios.post(`https://636047c43e8f65f283c6d66d.mockapi.io/api/lms/users`, data)
+            .then((res) => {
+                console.log(res)
+                setData({
+                    email: "",
+                    first_name: "",
+                    last_name: "",
+                    username: "",
+                    password: "",
+                    confirm_pass: ""
+                })
+                navigate("/login")
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+        } else {
+            alert('Password tidak sesuai')
+        }
+    }
+
     return (
         <>
             <Container>
@@ -20,31 +56,79 @@ const Regis = () => {
     
                 <Col md="6">
                 <div className="d-flex justify-content-center regis-body" id="regis-section">
-                    <Form>
+                    <Form method='post' onSubmit={registration}>
                     <h3>Register</h3>
     
                     <Form.Group className="mb-3" controlId="formInput">
-                        <Form.Control type="email" placeholder="Email..." />
+                        <Form.Control 
+                            onChange={e => {
+                                setData({...data,email:e.target.value})
+                            }}
+                            type="email"
+                            className="mail text-black"
+                            placeholder="Email..."
+                            value={data.email}
+                        />
                     </Form.Group>
     
                     <Form.Group className="mb-3" controlId="formInput">
-                        <Form.Control type="text" placeholder="First Name..." />
+                        <Form.Control 
+                            onChange={e => {
+                                setData({...data,first_name:e.target.value})
+                            }}
+                            type="text"
+                            className="name text-black"
+                            placeholder="First Name..."
+                            value={data.first_name}
+                        />
                     </Form.Group>
     
                     <Form.Group className="mb-3" controlId="formInput">
-                        <Form.Control type="text" placeholder="Last Name..." />
+                        <Form.Control 
+                            onChange={e => {
+                                setData({...data,last_name:e.target.value})
+                            }}
+                            type="text"
+                            className="name text-black"
+                            placeholder="Last Name..."
+                            value={data.last_name}
+                        />
                     </Form.Group>
                     
                     <Form.Group className="mb-3" controlId="formInput">
-                        <Form.Control type="username" placeholder="Username..." />
+                        <Form.Control 
+                            onChange={e => {
+                                setData({...data,username:e.target.value})
+                            }}
+                            type="username"
+                            className="user text-black"
+                            placeholder="Username..."
+                            value={data.username}
+                        />
                     </Form.Group>
     
                     <Form.Group className="mb-3" controlId="formInput">
-                        <Form.Control type="password" placeholder="Password..." />
+                        <Form.Control 
+                            onChange={e => {
+                                setData({...data,password:e.target.value})
+                            }}
+                            type="password"
+                            className="password text-black"
+                            placeholder="Password..."
+                            value={data.password}
+                        />
                     </Form.Group>
     
                     <Form.Group className="mb-3" controlId="formInput">
-                        <Form.Control type="password" placeholder="Confirm Password..." />
+                        <Form.Control
+                            onChange={e => {
+                                setData({...data,confirm_pass:e.target.value})
+                            }}
+                            type="password"
+                            className="password text-black"
+                            placeholder="Confirm Password..."
+                            value={data.confirm_pass}
+                        />
                     </Form.Group>
     
                     <p className="text-regis"><b><center>By signing up, you agree to our Terms, Privacy, Policy and Cookie Policy</center></b></p>
@@ -67,4 +151,4 @@ const Regis = () => {
     );
 };
   
-export default Regis;  
+export default Regis  
